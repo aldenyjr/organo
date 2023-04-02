107827d9e11bf291c5f6b8a170b66b99fea52065
Aldeny Jr <114154072+aldenyjr@users.noreply.github.com>
2023-04-01 23:28:06 -0300

    Projeto Finalizado

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,39 @@
-import './colaborador.css'
+import "./colaborador.css";
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+  const favoritar = () => {
+    aoFavoritar(colaborador.id);
+  };
 
-export default Colaborador
+  const propsfavorito = {
+    size: 25,
+    onClick: favoritar,
+  };
+
+  return (
+    <div className="colaborador">
+      <AiFillCloseCircle
+        className="deletar"
+        onClick={() => aoDeletar(colaborador.id)}
+        size={25}
+      />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+        <div className="favoritar">
+          {colaborador.favorito ? (
+            <AiFillHeart {...propsfavorito} color="ff0000" />
+          ) : (
+            <AiOutlineHeart {...propsfavorito} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,17 +1,49 @@
-import Colaborador from '../Colaborador'
-import './time.css'
+import Colaborador from "../Colaborador";
+import "./time.css";
+import hexToRgba from "hex-to-rgba";
 
-const Time = ({ time, colaboradores }) => {
-    return (
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
+  //   //   const [color, setColor] = useState(time.corSecundaria);
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
-            <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
-            </div>
-        </section>
+  //   const onChange = (e) => {
+  //     setColor(e.target.value);
+  //   };
 
+  return (
+    colaboradores.length > 0 && (
+      <section
+        className="time"
+        style={{
+          backgroundImage: "url(/imagens/fundo.png)",
+          backgroundColor: hexToRgba(time.cor, "0.3"),
+        }}
+      >
+        <input
+          type="color"
+          className="input-cor"
+          value={time.cor}
+          onChange={(e) => mudarCor(e.target.value, time.id)}
+          style={{
+            backgroundColor: hexToRgba(time.cor),
+          }}
+        />
+        <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+        <div className="colaboradores">
+          {colaboradores.map((colaborador, indice) => {
+            return (
+              <Colaborador
+                key={indice}
+                colaborador={colaborador}
+                corDeFundo={time.cor}
+                aoDeletar={aoDeletar}
+                aoFavoritar={aoFavoritar}
+              />
+            );
+          })}
+        </div>
+      </section>
     )
-}
+  );
+};
 
-export default Time
+export default Time;
